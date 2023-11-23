cbdc11fccde033f1e640e61b5181b3de0b64f4f5
XerHeartID <teolala.theodore@gmail.com>
2023-11-23 12:25:44 +0700

    .

---
+++ script.js
@@ -0,0 +1,81 @@
+let a = 5.2;
+document.write(`Variabel A = ${a} <br>`);
+let b = 5.6;
+document.write(`Variabel B = ${b} <br>`);
+let c = 6.4;
+document.write(`Variabel C = ${c} <br><br>`);
+
+// Math.round
+    document.write(`Math.round <br>`);
+
+    let round0 = Math.round(a);
+    console.log(round0);
+    document.write(`Hasil Pembulatan Var. A: ${round0} <br>`);
+
+    let round1 = Math.round(b);
+    console.log(round1);
+    document.write(`Hasil Pembulatan Var. B: ${round1} <br>`);
+
+    let round2 = Math.round(c);
+    console.log(round2);
+    document.write(`Hasil Pembulatan Var. C: ${round2} <br><br>`);
+
+// Math.ceil
+    document.write(`Math.ceil <br>`);
+
+    let ceil0 = Math.ceil(a);
+    console.log(ceil0);
+    document.write(`Hasil Pembulatan ke Atas Var. A: ${ceil0} <br>`);
+
+    let ceil1 = Math.ceil(b);
+    console.log(ceil1);
+    document.write(`Hasil Pembulatan ke Atas Var. B: ${ceil1} <br>`);
+
+    let ceil2 = Math.ceil(c);
+    console.log(ceil2);
+    document.write(`Hasil Pembulatan ke Atas Var. C: ${ceil2} <br><br>`);
+
+// Math.floor
+    document.write(`Math.floor <br>`);
+
+    let floor0 = Math.floor(a);
+    console.log(floor0);
+    document.write(`Hasil Pembulatan ke Bawah Var. A: ${floor0} <br>`);
+    
+    let floor1 = Math.floor(b);
+    console.log(floor1);
+    document.write(`Hasil Pembulatan ke Bawah Var. B: ${floor1} <br>`);
+    
+    let floor2 = Math.floor(c);
+    console.log(floor2);
+    document.write(`Hasil Pembulatan ke Bawah Var. C: ${floor2} <br><br>`);
+
+// Math.pow
+    document.write(`Math.pow <br>`)
+
+    let pow0 = Math.pow(6, 3);
+    console.log(pow0);
+    document.write(`Hasil Perpangkatan Pertama (6 Pangkat 3): ${pow0} <br>`);
+
+    let pow1 = Math.pow(2, 5);
+    console.log(pow1);
+    document.write(`Hasil Perpangkatan Kedua (2 Pangkat 5): ${pow1} <br>`);
+
+    let pow2 = Math.pow(4, 6);
+    console.log(pow2);
+    document.write(`Hasil Perpangkatan Ketiga (4 Pangkat 6): ${pow2} <br><br>`);
+
+// Math.sqrt
+    document.write(`Math.sqrt <br>`)
+
+    let sqrt0 = Math.sqrt(64);
+    console.log(sqrt0);
+    document.write(`Hasil Pengembalian Akar 64: ${sqrt0} <br>`);
+    
+    let sqrt1 = Math.sqrt(36);
+    console.log(sqrt1);
+    document.write(`Hasil Pengembalian Akar 36: ${sqrt1} <br>`);
+    
+    let sqrt2 = Math.sqrt(50);
+    console.log(sqrt2);
+    document.write(`Hasil Pengembalian Akar 50: ${sqrt2} <br>`);
